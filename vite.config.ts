f962b14383f260c82f515be611018f23b042ed7d
Chetan Dugar <chetandugar2003@gmail.com>
2026-06-12 01:01:44 +0530

    fix: remove github pages config for vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/Xtin-CryptoExchange/',
   plugins: [
     react(),
     tailwindcss(),
